6fab0a08ce133997b670554d3defe4a314d899e2
dyahdwihandayani <62831904+dyahdwihandayani@users.noreply.github.com>
2020-04-06 14:53:41 +0700

    Add files via upload

---
+++ InputOutput.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class InputOutput{
+public static void main (String [] args){
+	Scanner scanner = new Scanner(System.in);
+	String nama;
+	int jumlahSks;
+	double IPK;
+	
+	System.out.print("Masukan nama anda: ");
+	nama = scanner.nextLine();
+	System.out.println("Hai, "+nama);
+	
+		System.out.print("Masukan Jumlah SKS yang sudah ditempuhempuh : ");
+	nama = scanner.nextLine();
+	
+		System.out.print("Masukan IPK sementara: ");
+	nama = scanner.nextDouble();
+	
+		double jumlah = IPK * jumlahSks;
+	System.Out.print("Total Nilai ="+jumlah);
+}
+}
+	
+	
+
+}
+}
